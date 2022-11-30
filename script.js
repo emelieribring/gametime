c5671ac0f64d06eec76a55f5eb677e8fb8a5a3b4
felle64 <felle641@gmail.com>
2022-11-30 11:57:46 +0100

    kaos

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 let hero = document.getElementById("hero");
 let game = document.getElementById("game");
+let floor = document.getElementById("floor");
+const canvas = document.querySelector("canvas");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let l = canvas.getContext('2d');
+ 
 
 let bottom = 0;
 let left = 30;
@@ -25,16 +33,76 @@ document.addEventListener("keydown", (e) => {
 })
 
 
-let enemy = document.getElementById("enemy");
+//let enemy = document.getElementById("enemy");
 let y = 1;
 let gravity = -10;
 let wit
 
-let move = setInterval(function(){
+/*let move = setInterval(function(){
 
-    if (y>420) gravity = -gravity;
-    gravity += 3;
-    y += gravity;
+    if (y>620) gravity = -gravity;
+    gravity -= 1;
+    y -= gravity;
     enemy.style.top = (y) + 'px';
-},30)
+},30)*/
 
+
+let enemyId = 0;
+
+function createEnemy() {
+    enemyId++
+    
+    let enemy = document.createElement("div");
+    enemy.classList = "enemy";
+    let enemyTop = Math.round(Math.round(Math.random() * 500)/10)*10;
+    let enemyBottom = 0;
+    console.log(enemyBottom);
+    // Math.round(Math.floor(Math.random() * (500 - 1)/10)*10)
+    enemy.style.left = Math.round(Math.round(Math.random() * 700)/10)*10 + "px";
+
+    enemy.style.top = enemyTop + "px";
+    enemy.style.bottom = enemyBottom + "px";
+    enemy.id = enemyId;
+    let y = 1;
+    let gravity = -10;
+
+
+    let move = setInterval(() => {
+        //enemyTop += 5;
+        enemy.style.top = (y) + "px";
+        if (y>600) gravity =- gravity;
+            gravity -= 10;
+            y -= gravity;
+       
+
+        if (enemyBottom > bottom && enemyBottom < bottom + 150 && enemyTop === top) {
+            console.log("HIT");
+
+            let dead = setInterval(() => {
+                hero.style.backgroundColor = "red";
+
+                let resurect = setInterval(() => {
+                    hero.style.backgroundColor = "purple"
+                    clearInterval(dead)
+                }, 100)
+
+            }, 100)
+            
+        
+        }
+
+        if (enemyTop <= 0) {
+            clearInterval(move);
+            enemy.remove();
+            createEnemy();
+        }
+
+    }, 50)
+
+    game.appendChild(enemy);
+}
+
+createEnemy();
+if ( enemyId > 600){
+    createEnemy();
+} 
