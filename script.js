288b63cb97149488da78e7f3ddb26ff6568291f8
EMELIE RIBRING <emelieribring@hotmail.com>
2022-11-30 10:04:23 +0100

    test

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
+let hero = document.getElementById("hero");
+let game = document.getElementById("game");
+
+let bottom = 0;
+let left = 30;
+
+
+document.addEventListener("keydown", (e) => {
+    console.log ("e", e.key);
+
+    switch (e.key) {
+        case "d":
+        case "ArrowRight":
+            console.log("vi går upp");
+            left += 10;
+            hero.style.left = left + "px"
+        break;
+        case "a":
+        case "ArrowDown":
+            console.log("vi går ner");
+            left -= 10;
+            hero.style.left = left + "px"
+        break;
+    }
+})
+
+
+let enemy = document.getElementById("enemy");
+let y = 1;
+let gravity = 1;
+let wit
+
+let move = setInterval(function(){
+
+    if (y>420) gravity = -gravity;
+    gravity += 1;
+    y += gravity;
+    enemy.style.top = (y) + 'px';
+},30)
 
